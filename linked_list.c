e76b73f5fc88bea36835c9b6ed958b973f02eca1
Sumeet Sarkar <sumeetsarkar4@gmail.com>
2023-08-15 12:40:58 +0530

    linked_list refactor

--- linked_list.c
+++ linked_list.c
@@ -13,55 +13,63 @@ void ll_print(node *start);
 void ll_insert(node *start, const char *value);
 node *ll_find(node *start, const char *value);
 int ll_delete(node *start, const char *value); // deletes first found value
+node *create_new_node(const char *value);
 
 int main(int argc, char **argv) {
 
-    node list1 = { .value = "\\[__$head$__]\\", .next = NULL };
-    ll_print(&list1);
+    node *list1 = create_new_node("\\[__$head$__]\\");
+    ll_print(list1);
 
     printf("\n\n++ Adding more values");
-    ll_insert(&list1, "Red");
-    ll_insert(&list1, "Green");
-    ll_insert(&list1, "Blue");
+    ll_insert(list1, "Red");
+    ll_insert(list1, "Green");
+    ll_insert(list1, "Blue");
 
-    ll_print(&list1);
-    printf("\nSearching for Red\t%d", ll_find(&list1, "Red") != NULL);
-    printf("\nSearching for Green\t%d", ll_find(&list1, "Green") != NULL);
-    printf("\nSearching for Blue\t%d", ll_find(&list1, "Blue") != NULL);
-    printf("\nSearching for Yellow\t%d", ll_find(&list1, "Yellow") != NULL);
+    ll_print(list1);
+    printf("\nSearching for Red\t%d", ll_find(list1, "Red") != NULL);
+    printf("\nSearching for Green\t%d", ll_find(list1, "Green") != NULL);
+    printf("\nSearching for Blue\t%d", ll_find(list1, "Blue") != NULL);
+    printf("\nSearching for Yellow\t%d", ll_find(list1, "Yellow") != NULL);
     
-    ll_insert(&list1, "Cyan");
-    ll_insert(&list1, "Magenta");
-    ll_insert(&list1, "Yellow");
+    ll_insert(list1, "Cyan");
+    ll_insert(list1, "Magenta");
+    ll_insert(list1, "Yellow");
 
     printf("\n\n++ Adding more values");
-    ll_print(&list1);
+    ll_print(list1);
 
-    printf("\nSearching for Cyan\t%d", ll_find(&list1, "Cyan") != NULL);
-    printf("\nSearching for Magenta\t%d", ll_find(&list1, "Magenta") != NULL);
-    printf("\nSearching for Yellow\t%d", ll_find(&list1, "Yellow") != NULL);
+    printf("\nSearching for Cyan\t%d", ll_find(list1, "Cyan") != NULL);
+    printf("\nSearching for Magenta\t%d", ll_find(list1, "Magenta") != NULL);
+    printf("\nSearching for Yellow\t%d", ll_find(list1, "Yellow") != NULL);
 
-    printf("\n-- Deleting value Red\t%d", ll_delete(&list1, "Red"));
-    printf("\nSearching for Red\t%d", ll_find(&list1, "Red") != NULL);
-    ll_print(&list1);
+    printf("\n-- Deleting value Red\t%d", ll_delete(list1, "Red"));
+    printf("\nSearching for Red\t%d", ll_find(list1, "Red") != NULL);
+    ll_print(list1);
 
-    printf("\n-- Deleting value Cyan\t%d", ll_delete(&list1, "Cyan"));
-    printf("\n-- Deleting value Blue\t%d", ll_delete(&list1, "Blue"));
-    printf("\n-- Deleting value Green\t%d", ll_delete(&list1, "Green"));
-    printf("\n-- Deleting value Red\t%d", ll_delete(&list1, "Red"));
-    ll_print(&list1);
-    printf("\n-- Deleting value Magenta\t%d", ll_delete(&list1, "Magenta"));
-    printf("\n-- Deleting value Yellow\t%d", ll_delete(&list1, "Yellow"));
-    ll_print(&list1);
+    printf("\n-- Deleting value Cyan\t%d", ll_delete(list1, "Cyan"));
+    printf("\n-- Deleting value Blue\t%d", ll_delete(list1, "Blue"));
+    printf("\n-- Deleting value Green\t%d", ll_delete(list1, "Green"));
+    printf("\n-- Deleting value Red\t%d", ll_delete(list1, "Red"));
+    ll_print(list1);
+    printf("\n-- Deleting value Magenta\t%d", ll_delete(list1, "Magenta"));
+    printf("\n-- Deleting value Yellow\t%d", ll_delete(list1, "Yellow"));
+    ll_print(list1);
 
     printf("\n\n++ Adding more values");
-    ll_insert(&list1, "Cyan");
-    ll_insert(&list1, "Magenta");
-    ll_insert(&list1, "Yellow");
-    ll_print(&list1);
+    ll_insert(list1, "Cyan");
+    ll_insert(list1, "Magenta");
+    ll_insert(list1, "Yellow");
+    ll_print(list1);
 
     printf("\n");
     return 0;
+}
+
+node *create_new_node(const char *value) {
+   node *new_node = malloc(sizeof(node));
+   strcpy(new_node -> value, value);
+   new_node -> next = NULL;
+   return new_node;
 }
 
 void ll_print(node *start) {
@@ -82,13 +90,11 @@ void ll_insert(node *start, const char *value) {
         tmp = tmp -> next;
     }
 
-    node *new_node = malloc(sizeof(node));
+    node *new_node = create_new_node(value);
     if (new_node == NULL) {
         printf("\n!!Unable to allocate memory!!");
         return;
     }
-    strcpy(new_node -> value, value);
-    new_node -> next = NULL;
     tmp -> next = new_node;
 }
 

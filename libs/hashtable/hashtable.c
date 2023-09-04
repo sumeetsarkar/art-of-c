64d13180262f4b5557684a7cfed87d803347fa63
Sumeet Sarkar <sumeetsarkar4@gmail.com>
2023-09-05 00:37:42 +0530

    Fixed code indentation

--- libs/hashtable/hashtable.c
+++ libs/hashtable/hashtable.c
@@ -1,36 +1,36 @@
 #include "hashtable.h"
 
 typedef struct _entry {
-  char *key;
-  size_t key_len;
-  void *object;
-  entry *next;
+    char *key;
+    size_t key_len;
+    void *object;
+    entry *next;
 } entry;
 
 typedef struct _hash_table {
-  uint64_t size;
-  hash_function *hash_fn;
-  free_function *free_fn;
-  entry **entries;
+    uint64_t size;
+    hash_function *hash_fn;
+    free_function *free_fn;
+    entry **entries;
 } hash_table;
 
 hash_table *create_new(uint32_t size, hash_function *hash_fn, free_function *free_fn) {
-  hash_table *ht = malloc(sizeof(hash_table));
-  if (ht != NULL) {
-    ht -> size = size;
-    ht -> hash_fn = hash_fn;
-    ht -> free_fn = free_fn;
-    ht -> entries = calloc(ht -> size, sizeof(entry));
+    hash_table *ht = malloc(sizeof(hash_table));
+    if (ht != NULL) {
+        ht -> size = size;
+        ht -> hash_fn = hash_fn;
+        ht -> free_fn = free_fn;
+        ht -> entries = calloc(ht -> size, sizeof(entry));
 
-    if (ht -> entries == NULL) {
-      ht -> size = 0;
-      ht -> hash_fn = NULL;
-      ht -> free_fn = NULL;
-      free(ht);
-      ht = NULL;
+        if (ht -> entries == NULL) {
+          ht -> size = 0;
+          ht -> hash_fn = NULL;
+          ht -> free_fn = NULL;
+          free(ht);
+          ht = NULL;
+        }
     }
-  }
-  return ht;
+    return ht;
 }
 
 void destroy(hash_table *ht) {
--- libs/hashtable/test.c
+++ libs/hashtable/test.c
@@ -1,19 +1,19 @@
-#include "hashtable.h"
 #include <stdint.h>
 #include <stdlib.h>
+#include "hashtable.h"
 
 uint32_t hash_key(const char *key, size_t key_len);
 void free_object(void *object);
 
 int main(int argc, char **argv) {
-  hash_table *ht = create_new(1024, hash_key, free_object);
-  destroy(ht);
-  ht = NULL;
-  return EXIT_SUCCESS;
+    hash_table *ht = create_new(1024, hash_key, free_object);
+    destroy(ht);
+    ht = NULL;
+    return EXIT_SUCCESS;
 }
 
 uint32_t hash_key(const char *key, size_t key_len) {
-  return 0;
+    return 0;
 }
 
 void free_object(void *object) {

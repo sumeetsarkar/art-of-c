4ef8c29654551539e570133941cdcbf2115616b8
Sumeet Sarkar <sumeetsarkar4@gmail.com>
2023-09-06 23:43:59 +0530

    strdup to malloc new strings

--- strings/string_basic.c
+++ strings/string_basic.c
@@ -6,6 +6,7 @@ int main(int argc, char **argv) {
     // memory: allocated on stack
     char str1[] = "John Doe";
     printf("str1: %s\n", str1);
+
     str1[0] = 'a';
     printf("str1: %s\n", str1);
     
@@ -13,15 +14,24 @@ int main(int argc, char **argv) {
     char *str2 = malloc(16 * sizeof(char));
     strncpy(str2, "Clive John", 16);
     printf("str2: %s\n", str2);
+
     str2[0] = 'a';
     printf("str2: %s\n", str2);
 
     // memory: undefined
     char *str3 = "Blake harden";
     printf("str3: %s\n", str3);
-
     // following will cause a segmentation fault/ bus error
     // str3[0] = 'a';
+
+    // strndup mallocs new string with strlen + 1
+    // memcpy the string and returns a pointer to it, hence
+    // memory: allocated on heap with a pointer str4 on stack
+    char *str4 = strndup("Bob", 3);
+    printf("str4: %s\n", str4);
+
+    str4[0] = 'a';
+    printf("str4: %s\n", str4);
 
     return EXIT_SUCCESS;
 }
